9dcbd9fbe02308dbe78ce426efcc7ceb128d2276
li4236 <913327171@qq.com>
2016-05-12 10:30:53 +0800

    添加自定义CustomScrollView

---
+++ NOKIA/Nokia/src/main/java/com/nokia/view/CustomScrollView.java
@@ -0,0 +1,81 @@
+package com.nokia.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.ScrollView;
+
+/** 
+ * 能够兼容ViewPager的ScrollView 
+ * @Description: 解决了ViewPager在ScrollView中的滑动反弹问题 
+ */ 
+public class CustomScrollView extends ScrollView {  
+	// 滑动距离及坐标  
+    private float xDistance, yDistance, xLast, yLast; 
+    
+	private OnScrollListener onScrollListener;
+  
+    public CustomScrollView(Context context, AttributeSet attrs) {  
+        super(context, attrs);  
+    }  
+  
+    @Override  
+    public boolean onInterceptTouchEvent(MotionEvent ev) {  
+        switch (ev.getAction()) {  
+            case MotionEvent.ACTION_DOWN:  
+                xDistance = yDistance = 0f;  
+                xLast = ev.getX();  
+                yLast = ev.getY();  
+                break;  
+            case MotionEvent.ACTION_MOVE:  
+                final float curX = ev.getX();  
+                final float curY = ev.getY();  
+                  
+                xDistance += Math.abs(curX - xLast);  
+                yDistance += Math.abs(curY - yLast);  
+                xLast = curX;  
+                yLast = curY;  
+                  
+                if(xDistance > yDistance){  
+                    return false;  
+                }    
+        }  
+  
+        return super.onInterceptTouchEvent(ev);  
+    }  
+    
+    
+
+	/**
+	 * 设置滚动接口
+	 * @param onScrollListener
+	 */
+	public void setOnScrollListener(OnScrollListener onScrollListener) {
+		this.onScrollListener = onScrollListener;
+	}
+	
+
+	@Override
+	protected void onScrollChanged(int l, int t, int oldl, int oldt) {
+		super.onScrollChanged(l, t, oldl, oldt);
+		if(onScrollListener != null){
+			onScrollListener.onScroll(t);
+		}
+	}
+    /**
+	 * 
+	 * 滚动的回调接口
+	 * 
+	 * @author xiaanming
+	 *
+	 */
+	public interface OnScrollListener{
+		/**
+		 * 回调方法， 返回MyScrollView滑动的Y方向距离
+		 * @param scrollY
+		 * 				、
+		 */
+		public void onScroll(int scrollY);
+	}
+	
+}   
